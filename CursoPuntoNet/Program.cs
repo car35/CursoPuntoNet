756e508410b6c268f9b9109614f31bd7ff431186
caled <caled@LEDEZMA>
2025-12-14 19:23:26 -0500

    Agregar archivos de proyecto.

---
+++ CursoPuntoNet/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace CursoPuntoNet
+{
+	class Program
+	{
+        static void Main(string[] args)
+		{
+			int Pisos = 0;
+			int[] Array = new int[1];
+
+			//mostramos un mensaje y capturar elñ dato ingresado desde la consola.
+
+			Console.WriteLine("Ingrese el numero de pisos");
+			Pisos = Convert.ToInt16(Console.ReadLine());
+
+			for (int i = 1; i <= Pisos; i++)
+			{
+
+				int[] pascal = new int[i];
+
+				for (int j = Pisos; j < i; j--)
+				{
+					Console.Write("");
+				}
+				for (int k = 0; k < i; k++)
+				{
+					if (k == 0 || k == (i - 1))
+					{
+						pascal[k] = 1;
+
+					}
+					else
+					{
+						pascal[k]= Array[k] + Array[k-1];	
+					}
+
+					Console.Write('[' + pascal[k]+']');
+				}
+				Array = pascal;
+				Console.WriteLine(" ");	
+			}
+            Console.ReadLine();
+        }
+
+	}
+}
